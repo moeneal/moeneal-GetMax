8938f853019d194d99637aba6cfc0344496e9077
moeneal <momo.mneal@gmail.com>
2023-02-11 01:42:56 +0000

    took longer than should but max challenge done

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -1,3 +1,5 @@
+import java.util.stream.IntStream;
+
 public class GetMax {
     /**
      * This method should return the max value of arr (the largest int.)
@@ -7,6 +9,18 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+
+    
+        int tempMax =0;
+        for(int i = 0; i < arr.length; i++){
+            int tempVar = arr[i];
+if(tempVar < 0 && i == 0){
+    tempMax = tempVar;
+}
+if( tempVar > tempMax){
+    tempMax = tempVar;
+}
+        }
+        return tempMax;
     }
 }
